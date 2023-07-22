40fbd30f5194ceeb8131534b5d9e56a3149b813e
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-07-22 14:37:22 +0330

    Displaying Recipe Details

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,8 +8,9 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent {
   recipes : Recipe[] = [
+    new Recipe('A Test Recipe' , 'This is simply a test' , 'https://www.simplyrecipes.com/thmb/4_wTLA6Q_2o7dgNO-ze0w7oeX_w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg') ,
     new Recipe('A Test Recipe' , 'This is simply a test' , 'https://www.simplyrecipes.com/thmb/4_wTLA6Q_2o7dgNO-ze0w7oeX_w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg')
-    
+
   ] ;
 
   constructor(){
