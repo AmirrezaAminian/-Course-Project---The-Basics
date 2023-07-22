37cdb4c3084b0c629a8fbc707de97e734cd2d8da
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-07-22 13:33:26 +0330

    dding content to ecipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  recipes=[] ;
+  recipes : Recipe[] = [
+    new Recipe('A Test Recipe' , 'This is simply a test' , 'https://www.simplyrecipes.com/thmb/4_wTLA6Q_2o7dgNO-ze0w7oeX_w=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Homemade-Pizza-Dough-Lead-Shot-1b-ea13798d224048b3a28afb0936c9b645.jpg')
+    
+  ] ;
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+
+  }
 }
