2105639a41abf724aae2c91104acd417ebbaf1e7
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-07-29 13:02:59 +0330

    last commit maybe

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component , Input } from '@angular/core';
+import { Component , Input , Output , EventEmitter} from '@angular/core';
 import { Recipe } from '../../recipe.model';
 
 @Component({
@@ -8,5 +8,11 @@ import { Recipe } from '../../recipe.model';
 })
 export class RecipeItemComponent {
  @Input()  recipe : Recipe ; 
+ @Output() recipeSelected = new EventEmitter<void>();
+
+  
+ onSelected(){
+    this.recipeSelected.emit()
+ }
 
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component , OnInit } from '@angular/core';
+import { Component , OnInit ,  Output , EventEmitter } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,6 +7,9 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes : Recipe[] = [
     new Recipe('A Test Recipe' , 'This is simply a test' , 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg') ,
     new Recipe('A Test Recipe' , 'This is simply a test' , 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
@@ -19,5 +22,9 @@ export class RecipeListComponent {
 
   ngOnInit(){
 
+  }
+
+  onRecipeSelected(recipe: Recipe){
+      this.recipeWasSelected.emit(recipe);
   }
 }
